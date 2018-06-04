150ba1bb4302740867fea99781cc7aceaa459448
Dukihung <dukihung@gmail.com>
2018-06-04 19:58:34 +0200

    add unique/nullable to MySQL

--- Entwurf/Datenbank.sql
+++ Entwurf/Datenbank.sql
@@ -7,34 +7,38 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `Gruppe` (
     `GruppenNummer` INTEGER NOT NULL,
-    PRIMARY KEY (`GruppenNummer`)
+    PRIMARY KEY (`GruppenNummer`),
+    UNIQUE (`GruppenNummer`)
 );
 
 CREATE TABLE `Termin` (
     `Datum` DATE NOT NULL,
     `Zustand` CHAR(11) NOT NULL,
     `Startzeit` TIME NOT NULL,
-    `Endzeit` TIME NOT NULL,
-    `Bemerkung` VARCHAR(128) NOT NULL,
-    PRIMARY KEY (`Datum`)
+    `Endzeit` TIME,
+    `Bemerkung` VARCHAR(128),
+    PRIMARY KEY (`Datum`),
+    UNIQUE (`Datum`)
 );
 
 CREATE TABLE `Buchung` (
     `Gruppe` INTEGER NOT NULL,
     `Startzeit` TIME NOT NULL,
-    `Endzeit` TIME NOT NULL,
+    `Endzeit` TIME,
     `Raum` CHAR(15) NOT NULL,
     `Termin` DATE NOT NULL,
-    PRIMARY KEY (`Termin`)
+    PRIMARY KEY (`Termin`),
+    UNIQUE (`Gruppe`, `Termin`)
 );
 
 CREATE TABLE `Reservierung` (
     `Gruppe` INTERGER NOT NULL,
     `Termin` DATE NOT NULL,
-    PRIMARY KEY (`Gruppe`)
+    PRIMARY KEY (`Gruppe`),
+    UNIQUE (`Gruppe`, `Termin`)
 );
 
 ALTER TABLE `Buchung` ADD FOREIGN KEY (`Gruppe`) REFERENCES `Gruppe`(`GruppenNummer`);
 ALTER TABLE `Buchung` ADD FOREIGN KEY (`Termin`) REFERENCES `Termin`(`Datum`);
-ALTER TABLE `Reservierung` ADD FOREIGN KEY (`Gruppe`) REFERENCES `Termin`(`Datum`);
+ALTER TABLE `Reservierung` ADD FOREIGN KEY (`Gruppe`) REFERENCES `Gruppe`(`GruppenNummer`);
 ALTER TABLE `Reservierung` ADD FOREIGN KEY (`Termin`) REFERENCES `Termin`(`Datum`);
