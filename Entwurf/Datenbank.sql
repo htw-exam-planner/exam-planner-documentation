90a7fc60e7c0219dc3593255d0ea322f5685ed82
Dukihung <dukihung@gmail.com>
2018-06-11 17:50:04 +0200

    translate to englisch

--- Entwurf/Datenbank.sql
+++ Entwurf/Datenbank.sql
@@ -1,42 +1,42 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `Gruppe`;
-DROP TABLE IF EXISTS `Termin`;
-DROP TABLE IF EXISTS `Buchung`;
-DROP TABLE IF EXISTS `Reservierung`;
+DROP TABLE IF EXISTS `Group`;
+DROP TABLE IF EXISTS `Appointment`;
+DROP TABLE IF EXISTS `Booking`;
+DROP TABLE IF EXISTS `Reservation`;
 SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE TABLE `Gruppe` (
-    `GruppenNummer` INTEGER NOT NULL,
-    PRIMARY KEY (`GruppenNummer`)
+CREATE TABLE `Group` (
+    `GroupNumber` INTEGER NOT NULL,
+    PRIMARY KEY (`GroupNumber`)
 );
 
-CREATE TABLE `Termin` (
-    `Datum` DATE NOT NULL,
-    `Zustand` CHAR(11) NOT NULL,
-    `Startzeit` TIME NOT NULL,
-    `Endzeit` TIME NOT NULL,
-    `Bemerkung` VARCHAR(128),
-    PRIMARY KEY (`Datum`),
-    UNIQUE (`Datum`)
+CREATE TABLE `Appointment` (
+    `Date` DATE NOT NULL,
+    `State` CHAR(11) NOT NULL,
+    `StartTime` TIME NOT NULL,
+    `EndTime` TIME NOT NULL,
+    `Note` VARCHAR(128),
+    PRIMARY KEY (`Date`),
+    UNIQUE (`Date`)
 );
 
-CREATE TABLE `Buchung` (
-    `Gruppe` INTEGER NOT NULL,
-    `Startzeit` TIME NOT NULL,
-    `Endzeit` TIME,
-    `Raum` CHAR(15),
-    `Termin` DATE NOT NULL,
-    PRIMARY KEY (`Termin`)
+CREATE TABLE `Booking` (
+    `Group` INTEGER NOT NULL,
+    `StartTime` TIME NOT NULL,
+    `EndTime` TIME,
+    `Room` CHAR(15),
+    `Appoinment` DATE NOT NULL,
+    PRIMARY KEY (`Appoinment`)
 );
 
-CREATE TABLE `Reservierung` (
-    `Gruppe` INTERGER NOT NULL,
-    `Termin` DATE NOT NULL,
-    PRIMARY KEY (`Gruppe`),
-    UNIQUE (`Gruppe`, `Termin`)
+CREATE TABLE `Reservation` (
+    `Group` INTERGER NOT NULL,
+    `Appointments` DATE NOT NULL,
+    PRIMARY KEY (`Group`),
+    UNIQUE (`Group`, `Appointments`)
 );
 
-ALTER TABLE `Buchung` ADD FOREIGN KEY (`Gruppe`) REFERENCES `Gruppe`(`GruppenNummer`);
-ALTER TABLE `Buchung` ADD FOREIGN KEY (`Termin`) REFERENCES `Termin`(`Datum`);
-ALTER TABLE `Reservierung` ADD FOREIGN KEY (`Gruppe`) REFERENCES `Gruppe`(`GruppenNummer`);
-ALTER TABLE `Reservierung` ADD FOREIGN KEY (`Termin`) REFERENCES `Termin`(`Datum`);
+ALTER TABLE `Booking` ADD FOREIGN KEY (`Group`) REFERENCES `Group`(`GroupNumber`);
+ALTER TABLE `Booking` ADD FOREIGN KEY (`Appoinment`) REFERENCES `Appointment`(`Date`);
+ALTER TABLE `Reservation` ADD FOREIGN KEY (`Group`) REFERENCES `Group`(`GroupNumber`);
+ALTER TABLE `Reservation` ADD FOREIGN KEY (`Appointments`) REFERENCES `Appointment`(`Date`);
