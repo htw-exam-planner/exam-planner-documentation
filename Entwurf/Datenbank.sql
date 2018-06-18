1eaeb3ad1701da1b68d127f3711b6fb89aa95a36
Dukihung <dukihung@gmail.com>
2018-06-18 15:15:55 +0200

    implemented changes requested by @DevWurm

--- Entwurf/Datenbank.sql
+++ Entwurf/Datenbank.sql
@@ -1,40 +1,46 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `Group`;
-DROP TABLE IF EXISTS `Appointment`;
-DROP TABLE IF EXISTS `Booking`;
-DROP TABLE IF EXISTS `Reservation`;
+DROP TABLE IF EXISTS Group;
+DROP TABLE IF EXISTS Appointment;
+DROP TABLE IF EXISTS Booking;
+DROP TABLE IF EXISTS Reservation;
+DROP TABLE IF EXISTS State;
 SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE TABLE `Group` (
-    `GroupNumber` INTEGER NOT NULL,
-    PRIMARY KEY (`GroupNumber`)
+CREATE TABLE Group (
+    GroupNumber INTEGER NOT NULL,
+    PRIMARY KEY (GroupNumber)
 );
 
-CREATE TABLE `Appointment` (
-    `Date` DATE NOT NULL,
-    `State` CHAR(11) NOT NULL,
-    `StartTime` TIME NOT NULL,
-    `EndTime` TIME NOT NULL,
-    `Note` VARCHAR(128),
-    PRIMARY KEY (`Date`)
+CREATE TABLE Appointment (
+    Date DATE NOT NULL,
+    State INTEGER NOT NULL,
+    StartTime TIME NOT NULL,
+    EndTime TIME NOT NULL,
+    Note VARCHAR(128),
+    PRIMARY KEY (Date)
 );
 
-CREATE TABLE `Booking` (
-    `Group` INTEGER NOT NULL,
-    `StartTime` TIME NOT NULL,
-    `EndTime` TIME,
-    `Room` CHAR(15),
-    `Appoinment` DATE NOT NULL,
-    PRIMARY KEY (`Appoinment`)
+CREATE TABLE Booking (
+    Reservation INTEGER NOT NULL,
+    StartTime TIME NOT NULL,
+    EndTime TIME,
+    Room CHAR(15),
+    PRIMARY KEY (Reservation)
 );
 
-CREATE TABLE `Reservation` (
-    `Group` INTERGER NOT NULL,
-    `Appointment` DATE NOT NULL,
-    PRIMARY KEY (`Group`)
+CREATE TABLE Reservation (
+    Group INTEGER NOT NULL,
+    Appointment DATE NOT NULL,
+    PRIMARY KEY (Group)
 );
 
-ALTER TABLE `Booking` ADD FOREIGN KEY (`Group`) REFERENCES `Group`(`GroupNumber`);
-ALTER TABLE `Booking` ADD FOREIGN KEY (`Appoinment`) REFERENCES `Appointment`(`Date`);
-ALTER TABLE `Reservation` ADD FOREIGN KEY (`Group`) REFERENCES `Group`(`GroupNumber`);
-ALTER TABLE `Reservation` ADD FOREIGN KEY (`Appointment`) REFERENCES `Appointment`(`Date`);
+CREATE TABLE State (
+    Number INTEGER NOT NULL,
+    Name CHAR(12) NOT NULL,
+    PRIMARY KEY (Number)
+);
+
+ALTER TABLE Appointment ADD FOREIGN KEY (State) REFERENCES State(Number);
+ALTER TABLE Booking ADD FOREIGN KEY (Reservation) REFERENCES Reservation(Group);
+ALTER TABLE Reservation ADD FOREIGN KEY (Group) REFERENCES Group(GroupNumber);
+ALTER TABLE Reservation ADD FOREIGN KEY (Appointment) REFERENCES Appointment(Date);
